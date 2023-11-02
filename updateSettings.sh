f489a90b90710e1d0f1f68525f28bb9bb24d3393
mt-shke <michel.tcha@gmail.com>
2023-11-02 16:29:45 +0100

    update snippets

---
+++ updateSettings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo 'Need an user name parameter'
+    return
+else
+     base_dossier="/home/$1/.config/Code/User"
+
+fi
+
+
+cp settings.json $base_dossier/settings.json
+cp snippets.json $base_dossier/snippets/CustomSnippets.code-snippets.json
+cp keybindings.json $base_dossier/keybindings.json
+
+    
